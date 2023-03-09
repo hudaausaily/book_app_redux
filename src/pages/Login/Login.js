c688bc3ae54ea8b4d3b6c4cc5423ddf053c2b214
Ahmadzytoon <ahmadzytoon@outlook.com>
2023-03-09 15:12:40 +0300

    ahmad

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import './login.css';
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./login.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 export default function Login() {
   return (
-    <div className='holder'>
-        <header className='header'>
-            <Navbar />   
-            login
-        </header>
-    </div>  
-)
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        {/* ________________________________________ */}
+        
+          <div className="body">
+            <form id="form">
+              <h2 id="h2">login</h2>
+              <div className="qqq">
+                <input id="input" type="text" placeholder="Your email" />
+                <p id="p1" />
+              </div>
+              <div className="qqq">
+                <input
+                  id="input"
+                  type="password"
+                  placeholder="Enter password"
+                />
+                <p id="p1" />
+              </div>
+              <input
+                id="inputButtonLogin"
+                type="button"
+                defaultValue="login"
+              />
+            </form>
+          </div>
+        {/* ________________________________________ */}
+
+      </header>
+    </div>
+  );
 }
